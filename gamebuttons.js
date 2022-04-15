145d29bdbb9c78fe99f1760ee86b74f72b8fbb53
DoomsickIe <93813060+DoomsickIe@users.noreply.github.com>
2022-04-15 10:51:00 -0400

    Add files via upload

---
+++ gamebuttons.js
@@ -0,0 +1,18 @@
+function buttonhtml5() {
+      const transition_div = document.querySelector('.transition');
+    transition_div.classList.add('is-active')
+    setTimeout( () => {
+        window.location = "html5.html"
+    }, 300);}
+    function buttonflash() {
+      const transition_div = document.querySelector('.transition');
+    transition_div.classList.add('is-active')
+    setTimeout( () => {
+        window.location = "flash.html"
+    }, 300);}
+    function buttonother() {
+      const transition_div = document.querySelector('.transition');
+    transition_div.classList.add('is-active')
+    setTimeout( () => {
+        window.location = "other.html"
+    }, 300);}
