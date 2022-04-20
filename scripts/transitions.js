a9e0c0e3dc8dd0466357a204aaf42a3850632441
DoomsickIe <93813060+DoomsickIe@users.noreply.github.com>
2022-04-19 20:51:52 -0400

    Update transitions.js

--- scripts/transitions.js
+++ scripts/transitions.js
@@ -8,24 +8,10 @@ function Navbarclickindex() {
     const transition_div = document.querySelector('.transition');
     transition_div.classList.add('is-active')
     setTimeout( () => {
-<<<<<<< HEAD
         window.location = "index.html"
     }, 300);
 }
 
-=======
-        window.location = "../proxy.html"
-    }, 300);
-}
-
-function Navbarclickgames() {
-    const transition_div = document.querySelector('.transition');
-    transition_div.classList.add('is-active')
-    setTimeout( () => {
-        window.location = "../games.html"
-    }, 300);
-}
->>>>>>> 3ab3cf152d35cbf55da5abbf95a86dc1b14bc6a8
 function Navbarclickcontact() {
     const transition_div = document.querySelector('.transition');
     transition_div.classList.add('is-active')
@@ -37,11 +23,7 @@ function Navbarclickproxy() {
     const transition_div = document.querySelector('.transition');
     transition_div.classList.add('is-active')
     setTimeout( () => {
-<<<<<<< HEAD
-        window.location = "proxy.html"
-=======
-        window.location = "../index.html"
->>>>>>> 3ab3cf152d35cbf55da5abbf95a86dc1b14bc6a8
+        window.location = "../proxy.html"
     }, 300);
 }
 function Navbarclickpolls() {
